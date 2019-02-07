4dfd34835ee2d10a76c82a752bb0e5d19f0ca0af
Rene Elias <reneelias03@gmail.com>
2019-02-06 20:39:06 -0800

    functions.js and arrays.js  finished

--- node/arrays.js
+++ node/arrays.js
@@ -15,15 +15,32 @@ array[1] = 'hello';
 // for practice
 
 // use forEach
-
+const arrayTest = [9, 3, 4];
+// arrayTest[5] = 9;
+console.log(arrayTest);
+array.forEach((n) => {
+    console.log(n);
+});
 // use map
 
+//if there is only one parameter in function, parantheses are unecessary
+const newArray = arrayTest.map(i => i + 1);
+console.log(newArray);
+
 // use pop
+newArray.pop();
+console.log(newArray);
 
 // use push
+newArray.push(6);
+console.log(newArray);
 
 // use shift
+newArray.shift();
+console.log(newArray);
 
 // use unshift
+newArray.unshift(5);
+console.log(newArray);
 
 // use filter
--- node/functions.js
+++ node/functions.js
@@ -6,6 +6,7 @@ function hi() {
 hi();
 
 // arrow functions
+// use arrow functions instead
 const arrow = (test) => {
   console.log(test);
 }
@@ -15,5 +16,14 @@ arrow('hi')
 // for practice
 
 // make an arrow function with no block body
-
+const myArrow = (y) => y + 1;
+// {
+//   return y + 1;
+// }
+console.log(myArrow(8));
 // make a function that returns a function
+const functionFactory = () => () => console.log('This is weird');
+const a = functionFactory();
+a();
+const anotherFunction = functionFactory;
+anotherFunction()();
